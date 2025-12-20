a21d23bb86e1b80d5940741f9dd8001559764833
GriogrAbrahamayanIngo <grigor.abrahamyan@ingoarmenia.am>
2025-12-20 13:13:47 +0400

    improve post search functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 
 function App() {
+  const [searchInput, setSearchInput] = useState('');
+  
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Header searchInput={searchInput} setSearchInput={setSearchInput} />
+      <Home searchInput={searchInput} />
     </div>
   );
 }
